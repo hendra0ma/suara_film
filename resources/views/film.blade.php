7b885bd71bee24363ae5b1610f31d5ad97cdb6b7
isa-ardi <54489663+isa-ardi@users.noreply.github.com>
2022-10-24 16:01:43 +0700

    home

--- resources/views/film.blade.php
+++ resources/views/film.blade.php
@@ -102,6 +102,17 @@
         padding-left: 6rem !important;
         padding-right: 6rem !important;
     }
+
+    .image-poster {
+        width: 200px;
+        height: 300px;
+        object-fit: cover
+    }
+
+    .container-poster {
+        overflow-x: scroll;
+        flex-wrap: nowrap;
+    }
 </style>
 
 {{-- film-carousel --}}
@@ -182,12 +193,42 @@
     </button>
 </div>
 
-<div class="container-fluid custom mt-4">
+<div class="container-fluid custom mt-4 mb-5">
+
     {{-- list film --}}
     <section>
         <h1>Trending</h1>
-        
+        <div class="row g-3 container-poster">
+            <div class="col-auto">
+                <img class="image-poster" src="https://i.pinimg.com/564x/4c/80/ac/4c80ac0ccf18e1a198bc6b4120a43a79.jpg" alt="">
+            </div>
+            <div class="col-auto">
+                <img class="image-poster" src="https://i.pinimg.com/564x/76/79/fb/7679fb44d1fe6fedf03f637b6fd15079.jpg" alt="">
+            </div>
+            <div class="col-auto">
+                <img class="image-poster" src="https://i.pinimg.com/564x/a9/29/67/a92967108cb3cd316d75ee89c052d8b4.jpg" alt="">
+            </div>
+            <div class="col-auto">
+                <img class="image-poster" src="https://i.pinimg.com/564x/04/7e/a7/047ea77d4ab0eb6c18e83e25309e8f77.jpg" alt="">
+            </div>
+            <div class="col-auto">
+                <img class="image-poster" src="https://i.pinimg.com/736x/6d/bc/28/6dbc281e78eaa97de537744ac5aad3ae.jpg" alt="">
+            </div>
+            <div class="col-auto">
+                <img class="image-poster" src="https://i.pinimg.com/564x/d0/11/33/d0113352f4b921fc92d958e106daefe1.jpg" alt="">
+            </div>
+            <div class="col-auto">
+                <img class="image-poster" src="https://i.pinimg.com/564x/a1/88/e5/a188e568a782853d090c381ee38d79f3.jpg" alt="">
+            </div>
+            <div class="col-auto">
+                <img class="image-poster" src="https://i.pinimg.com/564x/a4/3d/27/a43d27c53e995321825f4bcb881006f5.jpg" alt="">
+            </div>
+            <div class="col-auto">
+                <img class="image-poster" src="https://i.pinimg.com/564x/cb/e0/80/cbe0805aea34cca87635e51a007598a7.jpg" alt="">
+            </div>
+        </div>
     </section>
+
 </div>
 
 
